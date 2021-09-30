9c66c8dfa11caf51a986f43f17d39f85b50f0f53
ExistentialLamp <89541058+ExistentialLamp@users.noreply.github.com>
2021-09-29 19:51:30 -0800

    a bit of cleaning up in quadratic()

--- quadratic.cpp
+++ quadratic.cpp
@@ -8,11 +8,15 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::sqrt;
 
 void quadratic(){
+    //constants for the equation
     float a;
     float b;
     float c;
+
+    //gets constants from user
     cout << "In the form ax^2 + bx + c" << endl << "What is the value of a" << endl;
     cin >> a;
     cout << "What is the value of b" << endl;
@@ -20,15 +24,21 @@ void quadratic(){
     cout << "What is the value of c" << endl;
     cin >> c;
 
-    float determinant = b * b + 4 * a * c;
-    cout << a << "x^2 + " << b << "x + " << c;
+    /*
+     * if less than 0, no solution. negative square roots are imaginary
+     * if 0, 1 solution. if determinant is 0, x = -b/2a.
+     * if greater than 0, two solutions. two solutions because positive and negative sqrt(determinant)
+     */
+    float determinant = b*b - 4*a*c;
+    cout <<"(" << a << ")x^2 + (" << b << ")x + (" << c << ")";
+    //
     if(determinant < 0){
         cout << " has no roots";
         return;
-    } else if(determinant) {
-        cout << " has a root at x = " << -b / (2 * a);
+    } else if(determinant ==  0) {
+        cout << " has a root at x = " << -b / (2*a);
     } else {
-        cout << " has two roots at x = " << (-b - std::sqrt(determinant)) / (2*a);
-        cout << " and x = " << (-b + std::sqrt(determinant)) / (2*a);
+        cout << " has two roots at x = " << (-b - sqrt(determinant)) / (2*a);
+        cout << " and x = " << (-b + sqrt(determinant)) / (2*a);
     }
 }
